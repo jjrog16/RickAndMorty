9381bab5c8a67f5f09d2aed8f484c8a43535e78d
jjrog16 <52580893+jjrog16@users.noreply.github.com>
2020-07-30 01:45:06 -0400

    Added nav host fragment

--- app/src/main/java/com/example/android/rickandmorty/MainActivity.kt
+++ app/src/main/java/com/example/android/rickandmorty/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.android.rickandmorty
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Set up Timber
+        Timber.plant(Timber.DebugTree())
     }
 }
