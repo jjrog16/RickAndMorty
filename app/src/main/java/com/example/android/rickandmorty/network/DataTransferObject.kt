bb26deff9bba8b797797b103d5299e90be01853b
jjrog16 <52580893+jjrog16@users.noreply.github.com>
2020-07-30 02:18:08 -0400

    Added DataTransferObject.kt

---
+++ app/src/main/java/com/example/android/rickandmorty/network/DataTransferObject.kt
@@ -0,0 +1,7 @@
+package com.example.android.rickandmorty.network
+
+/**
+ * DataTransferObjects go in this file. These are responsible for parsing responses from the server
+ * or formatting objects to send to the server. You should convert these to domain objects before
+ * using them.
+ */
