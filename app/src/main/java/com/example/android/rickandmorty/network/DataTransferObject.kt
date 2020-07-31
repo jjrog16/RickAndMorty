e43caa0e940910700a994ba7193a780e9ba7593f
jjrog16 <52580893+jjrog16@users.noreply.github.com>
2020-07-31 18:05:25 -0400

    Added NetworkCharacterContainer extension function to convert to network results to database characters

--- app/src/main/java/com/example/android/rickandmorty/network/DataTransferObject.kt
+++ app/src/main/java/com/example/android/rickandmorty/network/DataTransferObject.kt
@@ -1,7 +1,45 @@
 package com.example.android.rickandmorty.network
 
+import com.example.android.rickandmorty.database.DatabaseCharacter
+import com.squareup.moshi.JsonClass
+
 /**
  * DataTransferObjects go in this file. These are responsible for parsing responses from the server
  * or formatting objects to send to the server. You should convert these to domain objects before
  * using them.
  */
+
+@JsonClass(generateAdapter = true)
+data class NetworkCharacterContainer(val characters: List<NetworkCharacter>)
+
+@JsonClass(generateAdapter = true)
+data class NetworkCharacter(
+    var id: Int,
+    var name: String,
+    var status: String,
+    var species: String,
+    var type: String,
+    var gender: String,
+    var image: String,
+    var url: String,
+    var created: String
+)
+
+/**
+ * Convert Network results to database objects
+ */
+fun NetworkCharacterContainer.asDatabaseModel(): Array<DatabaseCharacter>{
+    return characters.map {
+        DatabaseCharacter(
+            id = it.id,
+            name = it.name,
+            status = it.status,
+            species = it.species,
+            type = it.type,
+            gender = it.gender,
+            image = it.image,
+            url = it.url,
+            created = it.created
+        )
+    }.toTypedArray()
+}
